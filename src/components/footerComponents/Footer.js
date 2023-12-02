12fe622b3e29ea42ac560cc6dc71bc9f9c8d8407
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2023-12-02 14:47:55 +0100

    ales-descomponent

---
+++ src/components/footerComponents/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer" />
+      </div>
+    )
+  }
+}
+
+export default Footer
