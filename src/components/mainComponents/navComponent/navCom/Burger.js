d523eb1dd9010e572152ee91a539f2aff467e3c1
Eugen <worittagebez@gmail.comssh-keygen -t rsa -b 4096 -C worittagebez@gmail.com>
2023-12-03 15:44:30 +0100

    show-menu

--- src/components/mainComponents/navComponent/navCom/Burger.js
+++ src/components/mainComponents/navComponent/navCom/Burger.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Menu from './Menu'
 
 const Burger = () => {
+
+//Данные строки помогают показать контент Menu при нажатий на кнопки
+  const [visible, setVisible] = useState(true)
+  const toggleVisibility = () => setVisible(!visible)
+///////////////////////////////////////////////////////
+  
+  
   
     return (
       <div>
-         <div className="nav__burger burger">
+         <div onClick={toggleVisibility} className="nav__burger burger">
+         
             <span className="burger__line" />
             <span className="burger__line" />
             <span className="burger__line" />
         </div>
+        {visible ? "" : <Menu/>}
+        {/*//////////////////////*/}
       </div>
     )
   }
